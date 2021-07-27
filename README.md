ff856e6ea2d8aca019b20fe287d4539e19b8f714
时振超 <shizc@mail.open.com.cn>
2021-07-27 15:56:34 +0800

    分布式配置中心

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Spring-Cloud-V2020.0.3
-Spring Cloud V 2020.0.3 学习，JDK16
+> ## Spring Cloud V2020.0.3 学习，JDK16
 
 https://docs.docker.com/engine/install/centos/
+#Spring Cloud 安全
+
+```sh
+keytool -genkeypair -alias mytestkey -keyalg RSA \
+  -dname"CN=Web Server,OU=Unit,O=Organization,L=City,S=State,C=US" \
+  -keypass changeme -keystore server.jks -storepass letmein
+```
+
--- client1/src/main/java/org/xpp/client1/controller/Client1Controller.java
+++ client1/src/main/java/org/xpp/client1/controller/Client1Controller.java
@@ -1,12 +1,14 @@
 package org.xpp.client1.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@RefreshScope
 public class Client1Controller {
     @Value("${client1.configValue:abc}")
     private String client1ConfigValue;
