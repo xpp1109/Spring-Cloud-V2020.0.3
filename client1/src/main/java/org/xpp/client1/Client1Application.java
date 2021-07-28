a22af8bf9ce82126fe381fd61b924ecc872dc275
时振超 <shizc@mail.open.com.cn>
2021-07-28 10:17:53 +0800

    链路跟踪

--- client1/src/main/java/org/xpp/client1/Client1Application.java
+++ client1/src/main/java/org/xpp/client1/Client1Application.java
@@ -1,9 +1,12 @@
 package org.xpp.client1;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author xpp
@@ -14,5 +17,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Client1Application {
     public static void main(String[] args) {
         SpringApplication.run(Client1Application.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
     }
 }
